e89903fe42fb4bb2214ba9262d5ef20e16e94d69
gfgggghhh <81595715+gfgggghhh@users.noreply.github.com>
2022-11-30 19:04:55 +0200

    Add files via upload
    Signed-off-by: gfgggghhh <81595715+gfgggghhh@users.noreply.github.com>

---
+++ Default.aspx.vb
@@ -0,0 +1,8 @@
+﻿
+Partial Class meyawo_public_html_Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = "dfgdfg"
+    End Sub
+End Class
